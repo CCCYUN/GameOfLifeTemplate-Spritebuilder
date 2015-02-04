cf36be7f112fb875ff85f778a9f2c0f01d26d530
CCCYUN <caoyun9006@gmail.com>
2015-02-03 20:28:48 -0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Creature.h
+++ GameOfLife.spritebuilder/Source/Creature.h
@@ -1,5 +1,5 @@
 //
-//  Creatures.h
+//  Creature.h
 //  GameOfLife
 //
 //  Created by Yun on 2/3/15.
@@ -9,13 +9,12 @@
 #import "CCSprite.h"
 
 @interface Creature : CCSprite
+
 // stores the current state of the creature
 @property (nonatomic, assign) BOOL isAlive;
 
 // stores the amount of living neighbors
 @property (nonatomic, assign) NSInteger livingNeighbors;
-
-
 
 - (id)initCreature;
 
