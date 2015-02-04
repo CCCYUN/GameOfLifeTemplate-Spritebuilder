64b60442c8e58814ffde6780c46b592a2fcdb4e0
CCCYUN <caoyun9006@gmail.com>
2015-02-03 21:10:51 -0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -85,13 +85,13 @@ static const int GRID_COLUMNS = 10;
 - (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
 {
     //get the row and column that was touched, return the Creature inside the corresponding cell
-    Creature *creature = nil;
+    Creature *touched_creature = nil;
     
     int column = touchPosition.x / _cellWidth;
     int row = touchPosition.y / _cellHeight;
-    creature = _gridArray[row][column];
+    touched_creature = _gridArray[row][column];
     
-    return creature;
+    return touched_creature;
 }
 
 - (void)evolveStep {
